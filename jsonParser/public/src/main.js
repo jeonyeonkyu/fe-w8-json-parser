3f32c0c3b7f676ff5a54c4cabfe6be6dc653285e
jeonyeonkyu <ehb6915@naver.com>
2021-03-30 22:14:53 +0900

    Add: tokenizer 함수 추가

--- jsonParser/public/src/main.js
+++ jsonParser/public/src/main.js
@@ -2,4 +2,6 @@
 import '../scss/main.scss';
 import '../scss/util.scss';
 
-console.log('115');
+const jsonData = '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
+console.log('123');
+
