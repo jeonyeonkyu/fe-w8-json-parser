abdd95fb9f2ea0e722f5b96ebdec10e93b5717d9
jeonyeonkyu <ehb6915@naver.com>
2021-03-31 23:32:21 +0900

    Add: entry 데이터 추가

--- jsonParser/public/src/main.js
+++ jsonParser/public/src/main.js
@@ -1,11 +1,25 @@
 
 import '../scss/main.scss';
 import '../scss/util.scss';
+import tokenizer from './tokenizer.js';
+import lexer from './lexer.js';
+import parser from './parser.js';
 
-<<<<<<< HEAD
-console.log('115');
-=======
-const jsonData = '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
-console.log('123');
+function load() {
+  const el = {
+    $button: document.querySelector('._button'),
+    $textarea: document.querySelector('._input_text'),
+    $printBox: document.querySelector('._print_box')
+  }
 
->>>>>>> 1b91f6ef7a771d33997bf74a50c6d1733f309930
+  function parseClickHandler() {
+    const tokens = tokenizer(el.$textarea.value);
+    const lexerTokens = lexer(tokens);
+    const parseData = parser(lexerTokens);
+    el.$printBox.innerText = JSON.stringify(parseData, null, ' ');
+  }
+
+  el.$button.addEventListener('click', parseClickHandler);
+}
+
+window.addEventListener('DOMContentLoaded', load);
