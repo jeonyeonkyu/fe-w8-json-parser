1604dda0dc92f75768377588f1cb35ec7c2e2c4f
jeonyeonkyu <ehb6915@naver.com>
2021-04-01 23:06:33 +0900

    Refactor: parser 안의 내부함수 분리하기

--- jsonParser/public/src/parser.js
+++ jsonParser/public/src/parser.js
@@ -1,63 +1,143 @@
+// function parser(tokens) {
+//   let i = 0;
+//   function arrayParser(parentNode = { type: 'array', child: [] }) {
+//     while (++i < tokens.length) {
+//       switch (tokens[i].value) {
+//         case '[':
+//           parentNode.child.push(arrayParser());
+//           break;
+//         case '{':
+//           parentNode.child.push(objectParser());
+//           break;
+//         case ',':
+//           break;
+//         case ']':
+//           return parentNode;
+//         default:
+//           parentNode.child.push(tokens[i]);
+//       }
+//     }
+//     return parentNode;
+//   }
+
+//   function objectParser(parentNode = { type: 'object', child: [] }) {
+//     let j = 0;
+//     let isKey = true;
+//     while (++i < tokens.length) {
+//       switch (tokens[i].value) {
+//         case '[':
+//           parentNode.child[j].value.propValue = arrayParser();
+//           break;
+//         case '{':
+//           parentNode.child[j].value.propValue = objectParser();
+//           break;
+//         case ',':
+//           break;
+//         case '}':
+//           return parentNode;
+//         case ':':
+//           isKey = false;
+//           break;
+//         default:
+//           if (isKey) {
+//             parentNode.child[j] = { value: { propKey: tokens[i] } };
+//           } else {
+//             parentNode.child[j++].value.propValue = tokens[i];
+//             isKey = true;
+//           }
+//       }
+//     }
+//     return parentNode;
+//   }
+
+//   switch (tokens[0].value) {
+//     case '[':
+//       return arrayParser();
+//     case '{':
+//       return objectParser();
+//     default:
+//       return tokens[0];
+//   }
+// }
+
+
 function parser(tokens) {
-  let i = 0;
-  function arrayParser(parentNode = { type: 'array', child: [] }) {
-    while (++i < tokens.length) {
-      switch (tokens[i].value) {
-        case '[':
-          parentNode.child.push(arrayParser());
-          break;
-        case '{':
-          parentNode.child.push(objectParser());
-          break;
-        case ',':
-          break;
-        case ']':
-          return parentNode;
-        default:
-          parentNode.child.push(tokens[i]);
-      }
-    }
-    return parentNode;
-  }
-
-  function objectParser(parentNode = { type: 'object', child: [] }) {
-    let j = 0;
-    let isKey = true;
-    while (++i < tokens.length) {
-      switch (tokens[i].value) {
-        case '[':
-          parentNode.child[j++].value.propValue = arrayParser();
-          break;
-        case '{':
-          parentNode.child[j++].value.propValue = objectParser();
-          break;
-        case ',':
-          break;
-        case '}':
-          return parentNode;
-        case ':':
-          isKey = false;
-          break;
-        default:
-          if (isKey) {
-            parentNode.child[j] = { value: { propKey: tokens[i] } };
-          } else {
-            parentNode.child[j++].value.propValue = tokens[i];
-            isKey = true;
-          }
-      }
-    }
-    return parentNode;
-  }
-
-  switch (tokens[0].value) {
+  const firstToken = tokens[0];
+  const nextTokens = tokens.slice(1);
+  switch (firstToken.value) {
     case '[':
-      return arrayParser();
+      return arrayParser(nextTokens);
     case '{':
-      return objectParser();
+      return objectParser(nextTokens);
     default:
-      return tokens[0];
+      return firstToken;
   }
 }
+
+function arrayParser(list, idx = 0, parentNode = { type: 'array', child: [] }) {
+  let nextIdx;
+  for (let i = idx; i < list.length - 1;) {
+    switch (list[i].value) {
+      case '[':
+        const arrayObj = { type: 'array', child: [] };
+        nextIdx = arrayParser(list, i + 1, arrayObj);
+        parentNode.child.push(arrayObj);
+        i = nextIdx;
+        break;
+      case '{':
+        const objectObj = { type: 'object', child: [] };
+        nextIdx = objectParser(list, i + 1, objectObj);
+        parentNode.child.push(objectObj);
+        i = nextIdx;
+        break;
+      case ',':
+        i++;
+        break;
+      case ']':
+        return i + 1;
+      default:
+        parentNode.child.push(list[i++]);
+    }
+  }
+  return parentNode;
+}
+
+function objectParser(list, idx = 0, parentNode = { type: 'object', child: [] }) {
+  let j = 0;
+  let nextIdx;
+  let isKey = true;
+  for (let i = idx; i < list.length - 1;) {
+    switch (list[i].value) {
+      case '[':
+        const arrayObj = { type: 'array', child: [] };
+        nextIdx = arrayParser(list, i + 1, arrayObj);
+        parentNode.child[j++].value.propValue = arrayObj;
+        i = nextIdx;
+        break;
+      case '{':
+        const objectObj = { type: 'object', child: [] };
+        nextIdx = objectParser();
+        parentNode.child[j++].value.propValue = objectObj;
+        i = nextIdx;
+        break;
+      case ':':
+        isKey = false;
+      case ',':
+        i++;
+        break;
+      case '}':
+        return i + 1;
+      default:
+        if (isKey) {
+          parentNode.child[j] = { value: { propKey: list[i++] } };
+        } else {
+          parentNode.child[j++].value.propValue = list[i++];
+          isKey = true;
+        }
+    }
+  }
+  return parentNode;
+}
+
 
 export default parser;
--- test.js
+++ test.js
@@ -34,70 +34,87 @@ function convertToMatchType(token) {
   return giveMeanObject[token];
 }
 
+
 function parser(tokens) {
-  if (!tokens[0]) return tokens;
-  let i = 0;
-  function arrayParser(parentNode = { type: 'array', child: [] }) {
-    while (++i < tokens.length) {
-      switch (tokens[i].value) {
-        case '[':
-          parentNode.child.push(arrayParser());
-          break;
-        case '{':
-          parentNode.child.push(objectParser());
-          break;
-        case ',':
-          break;
-        case ']':
-          return parentNode;
-        default:
-          parentNode.child.push(tokens[i]);
-      }
-    }
-    return parentNode;
-  }
-
-  function objectParser(parentNode = { type: 'object', child: [] }) {
-    let j = 0;
-    let isKey = true;
-    while (++i < tokens.length) {
-      switch (tokens[i].value) {
-        case '[':
-          parentNode.child[j++].value.propValue = arrayParser();
-          break;
-        case '{':
-          parentNode.child[j++].value.propValue = objectParser();
-          break;
-        case ',':
-          break;
-        case '}':
-          return parentNode;
-        case ':':
-          isKey = false;
-          break;
-        default:
-          if (isKey) {
-            parentNode.child[j] = { value: { propKey: tokens[i] } };
-          } else {
-            parentNode.child[j++].value.propValue = tokens[i];
-            isKey = true;
-          }
-      }
-    }
-    return parentNode;
-  }
-
-  switch (tokens[0].value) {
+  const firstToken = tokens[0];
+  const nextTokens = tokens.slice(1);
+  switch (firstToken.value) {
     case '[':
-      return arrayParser();
+      return arrayParser(nextTokens);
     case '{':
-      return objectParser();
+      return objectParser(nextTokens);
     default:
-      return tokens[0];
+      return firstToken;
   }
 }
 
+function arrayParser(list, idx = 0, parentNode = { type: 'array', child: [] }) {
+  let nextIdx;
+  for (let i = idx; i < list.length - 1;) {
+    switch (list[i].value) {
+      case '[':
+        const arrayObj = { type: 'array', child: [] };
+        nextIdx = arrayParser(list, i + 1, arrayObj);
+        parentNode.child.push(arrayObj);
+        i = nextIdx;
+        break;
+      case '{':
+        const objectObj = { type: 'object', child: [] };
+        nextIdx = objectParser(list, i + 1, objectObj);
+        parentNode.child.push(objectObj);
+        i = nextIdx;
+        break;
+      case ',':
+        i++;
+        break;
+      case ']':
+        return i + 1;
+      default:
+        parentNode.child.push(list[i++]);
+    }
+  }
+  return parentNode;
+}
+
+function objectParser(list, idx = 0, parentNode = { type: 'object', child: [] }) {
+  let j = 0;
+  let nextIdx;
+  let isKey = true;
+  for (let i = idx; i < list.length - 1;) {
+    switch (list[i].value) {
+      case '[':
+        const arrayObj = { type: 'array', child: [] };
+        nextIdx = arrayParser(list, i + 1, arrayObj);
+        parentNode.child[j++].value.propValue = arrayObj;
+        i = nextIdx;
+        break;
+      case '{':
+        const objectObj = { type: 'object', child: [] };
+        nextIdx = objectParser();
+        parentNode.child[j++].value.propValue = objectObj;
+        i = nextIdx;
+        break;
+      case ':':
+        isKey = false;
+      case ',':
+        i++;
+        break;
+      case '}':
+        return i + 1;
+      default:
+        if (isKey) {
+          parentNode.child[j] = { value: { propKey: list[i++] } };
+        } else {
+          parentNode.child[j++].value.propValue = list[i++];
+          isKey = true;
+        }
+    }
+  }
+  return parentNode;
+}
+
 const jsonData = '["1a3",[null,false,["1 1",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true]';
+// const jsonData = '{"easy" :  ["hello"]}';
 
 const tokens = tokenizer(jsonData);
 // console.dir(tokens, { depth: null });
