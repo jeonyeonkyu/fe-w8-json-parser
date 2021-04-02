4f94246c711986175a873b7aa6a72a0900cbf080
jeonyeonkyu <ehb6915@naver.com>
2021-04-02 12:15:51 +0900

    Refactor: 객체 만드는 로직 함수로 따로 분리 makeStructure()

--- jsonParser/public/src/parser.js
+++ jsonParser/public/src/parser.js
@@ -1,66 +1,3 @@
-// function parser(tokens) {
-//   let i = 0;
-//   function arrayParser(parentNode = { type: 'array', child: [] }) {
-//     while (++i < tokens.length) {
-//       switch (tokens[i].value) {
-//         case '[':
-//           parentNode.child.push(arrayParser());
-//           break;
-//         case '{':
-//           parentNode.child.push(objectParser());
-//           break;
-//         case ',':
-//           break;
-//         case ']':
-//           return parentNode;
-//         default:
-//           parentNode.child.push(tokens[i]);
-//       }
-//     }
-//     return parentNode;
-//   }
-
-//   function objectParser(parentNode = { type: 'object', child: [] }) {
-//     let j = 0;
-//     let isKey = true;
-//     while (++i < tokens.length) {
-//       switch (tokens[i].value) {
-//         case '[':
-//           parentNode.child[j].value.propValue = arrayParser();
-//           break;
-//         case '{':
-//           parentNode.child[j].value.propValue = objectParser();
-//           break;
-//         case ',':
-//           break;
-//         case '}':
-//           return parentNode;
-//         case ':':
-//           isKey = false;
-//           break;
-//         default:
-//           if (isKey) {
-//             parentNode.child[j] = { value: { propKey: tokens[i] } };
-//           } else {
-//             parentNode.child[j++].value.propValue = tokens[i];
-//             isKey = true;
-//           }
-//       }
-//     }
-//     return parentNode;
-//   }
-
-//   switch (tokens[0].value) {
-//     case '[':
-//       return arrayParser();
-//     case '{':
-//       return objectParser();
-//     default:
-//       return tokens[0];
-//   }
-// }
-
-
 function parser(tokens) {
   const firstToken = tokens[0];
   const nextTokens = tokens.slice(1);
@@ -74,18 +11,22 @@ function parser(tokens) {
   }
 }
 
-function arrayParser(list, idx = 0, parentNode = { type: 'array', child: [] }) {
+function makeStructure(dataType) {
+  return { type: dataType, child: [] };
+}
+
+function arrayParser(list, idx = 0, parentNode = makeStructure('array')) {
   let nextIdx;
   for (let i = idx; i < list.length - 1;) {
     switch (list[i].value) {
       case '[':
-        const arrayObj = { type: 'array', child: [] };
+        const arrayObj = makeStructure('array');
         nextIdx = arrayParser(list, i + 1, arrayObj);
         parentNode.child.push(arrayObj);
         i = nextIdx;
         break;
       case '{':
-        const objectObj = { type: 'object', child: [] };
+        const objectObj = makeStructure('object');
         nextIdx = objectParser(list, i + 1, objectObj);
         parentNode.child.push(objectObj);
         i = nextIdx;
@@ -102,22 +43,22 @@ function arrayParser(list, idx = 0, parentNode = { type: 'array', child: [] }) {
   return parentNode;
 }
 
-function objectParser(list, idx = 0, parentNode = { type: 'object', child: [] }) {
-  let j = 0;
+function objectParser(list, idx = 0, parentNode = makeStructure('object')) {
+  let j = -1;
   let nextIdx;
   let isKey = true;
   for (let i = idx; i < list.length - 1;) {
     switch (list[i].value) {
       case '[':
-        const arrayObj = { type: 'array', child: [] };
+        const arrayObj = makeStructure('array');
         nextIdx = arrayParser(list, i + 1, arrayObj);
-        parentNode.child[j++].value.propValue = arrayObj;
+        parentNode.child[j].value.propValue = arrayObj;
         i = nextIdx;
         break;
       case '{':
-        const objectObj = { type: 'object', child: [] };
-        nextIdx = objectParser();
-        parentNode.child[j++].value.propValue = objectObj;
+        const objectObj = makeStructure('object');
+        nextIdx = objectParser(list, i + 1, objectObj);
+        parentNode.child[j].value.propValue = objectObj;
         i = nextIdx;
         break;
       case ':':
@@ -129,15 +70,14 @@ function objectParser(list, idx = 0, parentNode = { type: 'object', child: [] })
         return i + 1;
       default:
         if (isKey) {
-          parentNode.child[j] = { value: { propKey: list[i++] } };
+          parentNode.child[++j] = { value: { propKey: list[i++] } };
         } else {
-          parentNode.child[j++].value.propValue = list[i++];
+          parentNode.child[j].value.propValue = list[i++];
           isKey = true;
         }
     }
   }
   return parentNode;
 }
-
 
 export default parser;
